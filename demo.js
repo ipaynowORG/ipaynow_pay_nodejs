335f02c4df77aef6d8d076d83f7d1225406ac602
张江南 <zhangjiangnan@ipaynow.cn>
2017-11-16 14:28:08 +0800

    1

--- demo.js
+++ demo.js
@@ -3,11 +3,11 @@
 //SDK依赖
 var sdk = require('./sdk');
 //AppId和AppKey
-var app = new sdk.App("150753082825470","8jTST7ywIBY0QQ3RlcxWEl08Xj9gaYyQ")
+var app = new sdk.App("xxxxxxxxxxx","xxxxxxxxxxxxxx")
 //商品信息: 名称,简称,价格(分),打折码
-var orderDetail = new sdk.OrderDetail("哈哈哈24","mhtOrderDetail","1","")
+var orderDetail = new sdk.OrderDetail("测试商品","mhtOrderDetail","1","")
 //微信主扫
-// sdk.wx_scan_05(app,orderDetail,"","https://op-tester.ipaynow.cn/paytest/notify","135488687224577449","");
+// sdk.wx_scan_05(app,orderDetail,"","https://op-tester.ipaynow.cn/paytest/notify","135536078696638618","");
 //订单查询
 // sdk.queryOrderScan05("PNELD5R4QL969",app,"05");
 //退款
